71fc544ec76d33aafcadf09d4d91925291b6c266
attx <attx@localhost>
2023-11-03 16:53:18 +0100

    Added units from Library manager

--- main.c
+++ main.c
@@ -1,100 +1,46 @@
-/*******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the Empty Application Example
-*              for ModusToolbox.
-*
-* Related Document: See README.md
-*
-*
-********************************************************************************
-* Copyright 2021-2023, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
-
-/*******************************************************************************
-* Header Files
-*******************************************************************************/
 #include "cyhal.h"
 #include "cybsp.h"
 
-/*******************************************************************************
-* Macros
-*******************************************************************************/
+#include "mtb_ml_model.h"
+
+/* Include model files */
+#include MTB_ML_INCLUDE_MODEL_FILE(MODEL_NAME)
+
+mtb_ml_model_t *image_class_obj;
+static MTB_ML_DATA_T *result_buffer;
+static int model_output_size;
 
 
-/*******************************************************************************
-* Global Variables
-*******************************************************************************/
+cy_rslt_t image_class_init(void)
+{
+    cy_rslt_t result;
 
+    mtb_ml_model_bin_t image_class_bin = {MTB_ML_MODEL_BIN_DATA(MODEL_NAME)};
 
-/*******************************************************************************
-* Function Prototypes
-*******************************************************************************/
+    /* Initialize the Neural Network */
+    result = mtb_ml_model_init(&image_class_bin, NULL, &image_class_obj);
+    if(CY_RSLT_SUCCESS != result)
+    {
+        return result;
+    }
 
+#if !COMPONENT_ML_FLOAT32
+    /* Set the q-factor */
+    mtb_ml_model_set_input_q_fraction_bits(image_class_obj, QFORMAT_VALUE);
+#endif
 
-/*******************************************************************************
-* Function Definitions
-*******************************************************************************/
+    mtb_ml_model_get_output(image_class_obj, &result_buffer, &model_output_size);
 
-/*******************************************************************************
-* Function Name: main
-********************************************************************************
-* Summary:
-* This is the main function for CPU. It...
-*    1.
-*    2.
-*
-* Parameters:
-*  void
-*
-* Return:
-*  int
-*
-*******************************************************************************/
+    return result;
+}
+
 int main(void)
 {
     cy_rslt_t result;
 
-#if defined (CY_DEVICE_SECURE)
-    cyhal_wdt_t wdt_obj;
-
-    /* Clear watchdog timer so that it doesn't trigger a reset */
-    result = cyhal_wdt_init(&wdt_obj, cyhal_wdt_get_max_timeout_ms());
-    CY_ASSERT(CY_RSLT_SUCCESS == result);
-    cyhal_wdt_free(&wdt_obj);
-#endif
-
     /* Initialize the device and board peripherals */
     result = cybsp_init();
+    result = image_class_init();
 
     /* Board init failed. Stop program execution */
     if (result != CY_RSLT_SUCCESS)
